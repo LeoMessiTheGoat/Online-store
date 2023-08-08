b2c274677107307afe77898f496cec7228fc8386
LeoMessiTheGoat <andreiflorescu271@gmail.com>
2023-08-08 17:25:35 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+//product provider import
+import ProductProvider from './Contexts/ProductContext.jsx';
+//sidebar provider
+import SidebarProvider from './Contexts/SidebarContext.jsx';
+import CartProvider from './Contexts/CartContext.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <SidebarProvider>
+      <CartProvider>
+        <ProductProvider>
+          <React.StrictMode>
+            <App />
+          </React.StrictMode>
+        </ProductProvider>
+      </CartProvider>
+    </SidebarProvider>
+  
+)
